b8762bd9afbe74d3ce27d393fb26eeab028bc5de
nikhil <nikz.kamble@hotmail.com>
2024-11-30 16:30:37 +1300

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/House-Schedule/'
+  base: '/house-schedule/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
